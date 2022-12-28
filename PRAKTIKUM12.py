c023415ff8902c3a3a0536960f3473a199ecb632
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-12-29 00:46:32 +0700

    Add files via upload

---
+++ PRAKTIKUM12.py
@@ -0,0 +1,30 @@
+print ("NAMA : abdul aziz")
+print("NIM : 312210022")
+
+#menghitung jumlah karakter
+kata = len("hello world")
+print(kata)
+
+#mengambil huruf terakhir
+kata =("Hello world")
+print(kata[-1])
+
+#mengambil huruf index ke-2 sampai ke -4
+kata =(" HELLO WORLD")
+print(kata[3:6])
+
+#menghilangkan spasi
+kata =("Hello world")
+print(kata.replace(" ", ""))
+
+#mengubah teks menjadi besar
+kata = ("hello world")
+print(kata.upper())
+
+#mengubah teks menjadi kecil
+kata = ("HELLO WORLD")
+print(kata.lower())
+
+#mengganti karakter "H" dengan karakter "J"
+kata = ("Hello world")
+print(kata.replace("H", "J"))
---
+++ latihan2.py
@@ -0,0 +1,7 @@
+print(" NAMA : Abdul aziz")
+print("NIM : 312210022" )
+print(20*"=")
+umur = 24
+txt = 'Hello, nama saya jhon, dan umur saya adalah {0} tahun'
+
+print(txt.format(umur))
